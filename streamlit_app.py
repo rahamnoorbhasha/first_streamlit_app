ae7df00e9464b8e7602a871bf13c3177911f82df
Rahamtullah <20529373+rahamnoorbhasha@users.noreply.github.com>
2022-07-16 09:41:30 +0530

    Update streamlit_app.py
    added breakfast items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('snowflake badge2 course')
+streamlit.header('Breakfast Menu')
+streamlit.text('Dosa chutney')
+streamlit.text('Idly chutney')
+streamlit.text('Poori curry')
