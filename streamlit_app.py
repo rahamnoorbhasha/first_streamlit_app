9f9761cb78bb895397ffe0743dc75260e6c1a5dc
Rahamtullah <20529373+rahamnoorbhasha@users.noreply.github.com>
2022-07-16 09:44:35 +0530

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('snowflake badge2 course')
-streamlit.header('Breakfast Menu')
+streamlit.header('🥗 Breakfast Menu')
 streamlit.text('Dosa chutney')
 streamlit.text('Idly chutney')
 streamlit.text('Poori curry')
