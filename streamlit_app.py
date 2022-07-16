eea4339f59d9b7c6783ebad46e9632b4df4861a8
Rahamtullah <20529373+rahamnoorbhasha@users.noreply.github.com>
2022-07-16 09:51:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,6 @@ streamlit.text('Poori curry')
 import pandas as pd
 
 df=pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
+streamlit.multiselect("pick some fruits : ", list(df.index))
 streamlit.dataframe(df)
